b7ffa8ebb29acd00b058782d502abec5a517fc14
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-03-02 12:07:18 -0500

    Users are now linked to jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -69,6 +69,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:description, :origin, :destination, :cost, :containers)
+      params.require(:job).permit(:description, :origin, :destination, :cost, :containers, :user_id)
     end
 end
