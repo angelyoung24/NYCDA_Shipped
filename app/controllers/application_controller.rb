a0631c8638c744a720eacadaa59165ae5b969c5e
Ming <minghaozhou123@gmail.com>
2018-03-05 17:43:50 -0500

    authentication for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate 
+  	unless user_signed_in? 
+  		flash[:message] = "You are not logged in"
+  		redirect_to "/users/sign_in"
+  	end
+  end
 end
