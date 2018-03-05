79712ae03e93a2b790446e950455a503f4377b9d
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-03-05 15:39:54 -0500

    Added paperclip and a bit of styling for the boat

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301214059) do
+ActiveRecord::Schema.define(version: 20180305195725) do
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "boats_jobs", id: false, force: :cascade do |t|
